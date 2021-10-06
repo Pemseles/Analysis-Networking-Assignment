58445ff104a6e6c3b28f8e45b410e01c3a2bac7c
Pemseles <TJBouwsema@gmail.com>
2021-10-06 18:09:06 +0200

    BookHelper.cs is done (NOT TESTED)
    just test this when the rest is done god help me

--- Networking/DistLibrary/LibBookHelper/BookHelper.cs
+++ Networking/DistLibrary/LibBookHelper/BookHelper.cs
@@ -23,14 +23,92 @@ namespace BookHelper
     // Note: Complete the implementation of this class. You can adjust the structure of this class.
     public class SequentialHelper
     {
-        public SequentialHelper()
+        public static string booksJsonPath = Path.GetFullPath(@"Books.json");
+
+        public void SequentialHelper()
         {
-            //todo: implement the body. Add extra fields and methods to the class if needed
+            while (true) {
+                int maxByte = newBookSock.Receive(buffer);
+                data = Encoding.ASCII.GetString(buffer, 0, maxByte);
+
+                try {
+                    string fullBooksJsonStr = System.IO.File.ReadAllText(booksJsonPath);
+                    BookData requestedBook = JsonDeserialize<BookData>(data);
+
+                    if (fullBooksJsonStr.Contains(requestedBook.Title.ToLower())) {
+                        // book was found; does not matter if borrowed or not, handled on client-side
+                        byte[] msgNew = AssembleMsg(MessageType.BookInquiryReply);
+                        newBookSock.Send(msgNew);
+                    }
+                    else {
+                        // book was not found; sends back message 
+                        byte[] msgNew = AssembleMsg(MessageType.NotFound);
+                        newBookSock.Send(msgNew);
+                    }
+                }
+                catch {
+                    // error during message recieving (not serialised etc)
+                    byte[] msgNew = AssembleMsg(MessageType.Error);
+                    newBookSock.Send(msgNew);
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+
+        public static byte[] AssembleMsg(enum messageTypeEnum) {
+            if (messageTypeEnum == MessageType.NotFound) {
+                // build message if book not found
+                Message replyJsonData = new Message {
+                    Type = MessageType.NotFound,
+                    Content = "From BookHelper server: Book not found\n"
+                }
+                string replyBookNotFound = JsonSerializer.Serialize(replyJsonData);
+                byte[] msgNew = Encoding.ASCII.GetBytes(replyBookNotFound);
+                return msgNew;
+                
+            }
+            else if (messageTypeEnum == MessageType.BookInquiryReply) {
+                // build message if book found
+                Message replyJsonData = new Message {
+                    Type = MessageType.BookInquiryReply,
+                    Content = requestedBook
+                }
+                string replyBookFound = JsonSerializer.Serialize(replyJsonData);
+                byte[] msgNew = Encoding.ASCII.GetBytes(replyBookFound);
+                return msgNew;
+            }
+            else if (messageTypeEnum == MessageType.Error) {
+                // build message if error occured
+                Message replyJsonData = new Message {
+                    Type = MessageType.Error,
+                    Content = "From Bookhelper server: error occured during booksearch\n"
+                }
+                string replyError = JsonSerializer.Serialize(replyJsonData);
+                byte[] msgNew = Encoding.ASCII.GetBytes(replyError);
+                return msgNew;
+            }
         }
 
         public void start()
         {
-            //todo: implement the body. Add extra fields and methods to the class if needed
+            string ip = "127.0.0.1";
+            int port = 32000;
+            
+            byte[] buffer = new byte[1000];
+            byte[] msg = Encoding.ASCII.GetBytes("From BookHelper server: Your message was delivered\n");
+            string data = null;
+
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint localEndpoint = new IPEndPoint(ipAddress, port);
+
+            Socket bookSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            bookSock.bind(localEndpoint);
+            sock.Listen(5);
+            Console.WriteLine("\nWaiting for main server...");
+            Socket newBookSock = bookSock.Accept();
+
+            SequentialHelper();
         }
     }
 }
